87a5c808762ab0ca00afc84182233ba1733fe15d
Avraham Micael <avrahammicael@protonmail.com>
2023-03-14 18:41:40 -0300

    404 page

--- src/components/ErrorBoundary/index.tsx
+++ src/components/ErrorBoundary/index.tsx
@@ -1,5 +1,5 @@
 import React, { Component, ErrorInfo, ReactNode } from "react";
-import WarningSVG from "./WarningSVG";
+import WarningSVG from "../WarningSVG";
 
 type Props = { children: ReactNode };
 
---
+++ src/pages/404.tsx
@@ -0,0 +1,5 @@
+import WarningSVG from "@/components/WarningSVG";
+
+const NotFound = () => <WarningSVG message="Page Not Found"/>;
+
+export default NotFound;
